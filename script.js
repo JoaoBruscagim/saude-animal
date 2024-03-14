d5380a2f18cc057ccab2664d9cd11ff2c7745380
JoaoBruscagim <olucasjoaolv@hotmail.com>
2024-03-13 21:01:44 -0300

    Bug resolvido

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ function menuOn() {
         if(navbarMenu.classList.contains('active')){
             navbarMenu.classList.remove('active')
             navbarMenu.classList.toggle('inactive')
+            setTimeout(() => {
+                navbarMenu.classList.remove('inactive')
+             },1000);
+
         } else if(navbarMenu.classList.contains('inactive')){
             navbarMenu.classList.toggle('active')
-            navbarMenu.classList.remove('inactive')
         } else {
             navbarMenu.classList.toggle('active')
         }
