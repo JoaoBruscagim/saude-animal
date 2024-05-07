95581daa5589d5d6f615e0ecbab95c7fca4a4044
JoaoBruscagim <olucasjoaolv@hotmail.com>
2024-05-07 11:27:47 -0300

    Onde Estamos

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 function menuOn() {
-    var navbarMenu = document.querySelector('.navbar-menu');
+  var navbarMenu = document.querySelector('.navbar-menu');
 
-        if(navbarMenu.classList.contains('active')){
-            navbarMenu.classList.remove('active')
-            navbarMenu.classList.toggle('inactive')
-            setTimeout(() => {
-                navbarMenu.classList.remove('inactive')
-             },1000);
+  if (navbarMenu.classList.contains('active')) {
+    navbarMenu.classList.remove('active')
+    navbarMenu.classList.toggle('inactive')
+    setTimeout(() => {
+      navbarMenu.classList.remove('inactive')
+    }, 1000);
 
-        } else if(navbarMenu.classList.contains('inactive')){
-            navbarMenu.classList.toggle('active')
-        } else {
-            navbarMenu.classList.toggle('active')
-        }
+  } else if (navbarMenu.classList.contains('inactive')) {
+    navbarMenu.classList.toggle('active')
+  } else {
+    navbarMenu.classList.toggle('active')
+  }
 
 }
-
 
 var onTop = false;
 var nav = document.querySelector('.navbar');
 window.onscroll = function (evt) {
 
- if(window.scrollY > 100 && !onTop) {
-   nav.classList.add('onTop');
-   onTop = true;
- }
-  
- if(window.scrollY < 100 && onTop) {
-   nav.classList.remove('onTop');
-   onTop = false;
- }
+  if (window.scrollY > 100 && !onTop) {
+    nav.classList.add('onTop');
+    onTop = true;
+  }
+
+  if (window.scrollY < 100 && onTop) {
+    nav.classList.remove('onTop');
+    onTop = false;
+  }
 };
